c5282ecfb59d51da52264f29ded3addc0f02965c
Denis Stanovoy <den.stanovoy@gmail.com>
2023-11-08 22:09:44 +0300

    Добавлен SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,16 @@ import Ptag from '@/components/Ptag/Ptag';
 import Tag from '@/components/Tag/Tag';
 import Rating from '@/components/Rating/Rating';
 import { withLayout } from '@/layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from 'interfaces/menu.interfce';
 
-const Home: FC = () => {
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
+
+const Home: FC<HomeProps> = ({ menu }) => {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -24,8 +32,31 @@ const Home: FC = () => {
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((elem) => {
+          return (
+            <li key={elem._id.secondCategory}>
+              {elem._id.secondCategory}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
